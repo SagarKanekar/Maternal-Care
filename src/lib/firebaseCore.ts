941270b100d05d1641cb6e433c69e34d3b0c0b42
Sagar <sagarkanekar2004@gmail.com>
2026-03-22 21:27:37 +0530

    corrected projectid

--- src/lib/firebaseCore.ts
+++ src/lib/firebaseCore.ts
@@ -5,7 +5,7 @@ import { getFirestore } from "firebase/firestore";
 const firebaseConfig = {
   apiKey: "AIzaSyDPQMTVi3YHYI5TEEfMIoGGIZkgILRM7bk",
   authDomain: "pregnancy-belt-fb490.firebaseapp.com",
-  projectId: "https://pregnancy-belt-fb490-default-rtdb.firebaseio.com",
+  projectId: "pregnancy-belt-fb490",
   storageBucket: "pregnancy-belt-fb490.firebasestorage.app",
   messagingSenderId: "621588747978",
   appId: "1:621588747978:web:ffe72b0614bdeb82aa0633",
